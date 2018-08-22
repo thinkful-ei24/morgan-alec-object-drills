432c3446118caad2668c20bb0f84a95c28d79aa4
Morgan Newman <mpnewman95@gmail.com>
2018-08-22 13:13:39 -0700

    Scratch paper to help

---
+++ scratch.js
@@ -0,0 +1,12 @@
+// We have an array of hero objects
+// We want to find the hero object that matches our query of key, value pairs
+
+// .find is being passed a hero object
+  // Check if it has the key, value pairs in query
+  // If it does, return it
+
+// For all the keys in our query
+  // Check if the query key exists in our hero
+    // If it does, 
+      // Check the value and return true if the value matches
+  // Else if it doesn't have the query key, we return false
