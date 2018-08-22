bea4f587ea4774e6c534a08c0fadea0652c482d3
aleclivinghouse <aleclivinghouse@yahoo.com>
2018-08-22 08:46:22 -0700

    update object

--- object-creator.js
+++ object-creator.js
@@ -6,3 +6,11 @@ const createMyObject = function() {
     sayHello: function() { return 'hello'; }
   };
 };
+
+const updateObject = function(obj){
+  obj.foo = 'foot';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
+}
