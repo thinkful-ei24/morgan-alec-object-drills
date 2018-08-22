56bf7356585f3e5103113fe213b3d3dcddb8efa0
Morgan Newman <mpnewman95@gmail.com>
2018-08-22 08:49:57 -0700

    completed personMaker()

--- object-creator.js
+++ object-creator.js
@@ -13,4 +13,21 @@ const updateObject = function(obj){
   obj.bizz = 'bizz';
   obj.bang = 'bang';
   return obj;
+};
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function() {
+      return `${this.firstName} ${this.lastName}`;
+    }
+  };
+  return person;
 }
+
+let person = personMaker();
+
+console.log(person.fullName());
